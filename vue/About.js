6a7f9d7926cb951ec53accddfe8d87eabc1117b7
Aurore VERNET <aurore.vernet@live.com>
2024-04-10 21:58:42 +0200

    style skills

--- vue/About.js
+++ vue/About.js
@@ -1,5 +1,8 @@
 export default {
     template:
     `
+    <article>
+    
+    </article>
     `
 }
--- vue/Skills.js
+++ vue/Skills.js
@@ -1,7 +1,6 @@
 export default {
     template:
     `
-    <h2 class="title">COMPÉTENCES TECHNIQUES</h2>
     <section id=legend>
         <div v-for="legend in legends">
             <img :src="legend.path" :alt="legend.alt">
@@ -9,71 +8,45 @@ export default {
         </div>
     </section>
     <section id="skills-items">
-        <article>
+        <article id="skills-languages">
             <h3 class="title">LANGAGES</h3>
             <div>
-                <span v-for="language in languages.slice(0,2)">
-                <a :href="language.link" target="_blank">
-                    <img :src="language.path" :alt="language.alt">
-                </a>
-            </span>
-            </div>
-           <div>
-                <span v-for="language in languages.slice(2,4)">
-                    <a :href="language.link" target="_blank"><
-                        <img :src="language.path" :alt="language.alt"
-                    </a>
-                </span>
-            </div>
-            <div>
-                <span v-for="language in languages.slice(4,6)">
+                <span v-for="language in languages">
                     <a :href="language.link" target="_blank">
-                        <img :src="language.path" :alt="language.alt">
+                        <img :src="language.path" :alt="language.alt" :class="language.class">
                     </a>
                 </span>
             </div>
         </article>
-        <article>
+        <article id="skills-tools">
             <h3 class="title">OUTILS</h3>
             <div>
-                <span v-for="tool in tools.slice(0,4)">
-                    <img :src="tool.path" :alt="tool.alt">
+                <span v-for="tool in tools">
+                    <img :src="tool.path" :alt="tool.alt" :class="tool.class">
                 </span>
             </div>
-            <div>
-                <span v-for="tool in tools.slice(4,8)">
-                    <img :src="tool.path" :alt="tool.alt">
-                </span>
-            </div>
-            <div>
-            <span v-for="tool in tools.slice(8,9)">
-                <img :src="tool.path" :alt="tool.alt" :class="tool.class">
-            </span>
-        </div>
         </article>
-        <article>
+        <article id="skills-frameworks">
             <h3 class="title">FRAMEWORKS</h3>
             <div>
                 <span v-for="framework in frameworks">
                     <a :href="framework.link" target="u_blank">
-                        <img :src="framework.path" :alt="framework.alt">
+                        <img :src="framework.path" :alt="framework.alt" :class="framework.class">
                     </a>
                 </span>
             </div>
         </article>
-    </section>
-    <section id="skills-items-2">
-        <article>   
+        <article class="skills-db">   
             <h3 class="title">BASE DE DONNÉES</h3>
             <div>
                 <span v-for="database in databases">
-                    <img :src="database.path" :alt="database.alt"
+                    <img :src="database.path" :alt="database.alt" :class="database.class"
                 </span>
             </div>
         </article>
     </section>
     `,
-    props: ['path', 'alt', 'title'],
+    props: ['path', 'alt', 'title', 'class'],
     data() {
         return {
             legends: [
@@ -97,84 +70,96 @@ export default {
                 {
                     path: './img/skills/logo-html.svg',
                     alt: 'Logo HTML',
-                    link: 'https://auroreo.github.io/projet-bonappetit/'
+                    link: 'https://auroreo.github.io/projet-bonappetit/',
+                    class: 'small-logo'
                 },
                 {
                     path: './img/skills/logo-css.svg',
-                    alt: 'Logo CSS'
+                    alt: 'Logo CSS',
+                    class: 'small-logo'
                 },
                 {
                     path: './img/skills/logo-python.svg',
-                    alt: 'Logo Python'
+                    alt: 'Logo Python',
+                    class: 'small-logo'
                 },
                 {
                     path: './img/skills/logo-js.svg',
                     alt: 'Logo JS',
-                    link: 'https://auroreo.github.io/shifumi_overwatch/'
+                    link: 'https://auroreo.github.io/shifumi_overwatch/',
+                    class: 'small-logo'
                 },
                 {
                     path: './img/skills/logo-php.svg',
-                    alt: 'Logo PHP'
+                    alt: 'Logo PHP',
+                    class: 'small-logo'
                 }
             ],
             tools: [
                 {
                     path: './img/skills/logo-figma.svg',
-                    alt: 'Logo Figma'
+                    alt: 'Logo Figma',
+                    class: 'small-logo'
                 },
                 {
                     path: './img/skills/logo-illustrator.svg',
-                    alt: 'Logo Illustrator'
+                    alt: 'Logo Illustrator',
+                    class: 'small-logo'
                 },
                 {
                     path: './img/skills/logo-premierepro.svg',
-                    alt: 'Logo Premiere Pro'
+                    alt: 'Logo Premiere Pro',
+                    class: 'small-logo'
                 },
                 {
                     path: './img/skills/logo-vscode.svg',
-                    alt: 'Logo VSCode'
+                    alt: 'Logo VSCode',
+                    class: 'small-logo'
                 },
                 {
                     path: './img/skills/logo-wordpress.svg',
-                    alt: 'Logo WordPress'
+                    alt: 'Logo WordPress',
+                    class: 'small-logo'
                 },
                 {
                     path: './img/skills/logo-photoshop.svg',
-                    alt: 'Logo Photoshop'
+                    alt: 'Logo Photoshop',
+                    class: 'small-logo'
                 },
                 {
                     path: './img/skills/logo-indesign.svg',
-                    alt: 'Logo inDesign'
+                    alt: 'Logo inDesign',
+                    class: 'small-logo'
                 },
                 {
                     path: './img/skills/logo-trello.svg',
-                    alt: 'Logo Trello'
+                    alt: 'Logo Trello',
+                    class: 'small-logo'
                 },
                 {
                     path: './img/skills/logo-phpmyadmin.svg',
                     alt: 'Logo phMyAdmin',
                     class: 'big-logo'
                 },
-                {
-                    path: './img/skills/logo-figma.svg',
-                    alt: 'Logo Figma'
-                }
             ],
             frameworks: [
                 {
                     path: './img/skills/logo-vuejs.svg',
                     alt: 'Logo Vue.js',
-                    link: 'https://auroreo.github.io/ranking_of_films/'
+                    link: 'https://auroreo.github.io/ranking_of_films/',
+                    class: 'small-logo'
                 },
                 {
                     path: './img/skills/logo-bootstrap.svg',
                     alt: 'Logo Bootstrap',
+                    class: 'small-logo'
                 },
             ],
             databases: [
                 {
                     path: './img/skills/logo-mysql.svg',
-                    alt: 'Logo MySQL'
+                    alt: 'Logo MySQL',
+                    class: 'big-logo'
                 }
             ]
         }
